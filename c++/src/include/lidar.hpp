913fcb9ff6543d5cb7219929f38e448d8fc596b5
Aleix Segura <83470660+aleixsegura@users.noreply.github.com>
2025-07-03 21:48:48 +0200

    init: project

---
+++ c++/src/include/lidar.hpp
@@ -0,0 +1,26 @@
+#ifndef PROCESSING_HPP
+#define PROCESSING_HPP
+
+#include <string>
+#include <functional>
+
+
+const float LIDAR_GNSS_OFFSET_X = -0.036;
+const float LIDAR_GNSS_OFFSET_Y = 0.0;
+const float LIDAR_GNSS_OFFSET_Z = 0.69;
+const int LIDAR_POINT_DIST_MAX = 4;
+
+struct LidarPoint {
+    double timestamp;
+    double x, y, z;
+    double intensity;
+};
+
+struct TransformedPoint {
+    double x, y, z;
+    double intensity;
+};
+
+void ProcessLidarPoints(const std::string& filepath, const std::string& gnss_data_dir, const std::string& imu_data_dir);
+
+#endif
---
+++ c++/src/main.cpp
@@ -0,0 +1,28 @@
+#include "include/gnss.hpp"
+#include "include/lidar.hpp"
+#include <string>
+#include <filesystem>
+#include <iostream>
+#include <chrono>
+
+int main(int argc, char** argv) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::string lidar_model = argv[1];
+    std::string track_type  = argv[2];
+
+    const std::string gnss_data_path    = "../../" + lidar_model + "/" + track_type + "/gnss_" + track_type + ".txt";
+    const std::string imu_data_path     = "../../" + lidar_model + "/" + track_type + "/imu_" + track_type + ".txt";
+    const std::string lidar_points_path = "../../" + lidar_model + "/" + track_type + "/lidar_" + track_type + ".txt";
+
+    
+    ProcessLidarPoints(lidar_points_path, gnss_data_path, imu_data_path);
+    
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "Duration of the program = " << duration.count() << " seconds\n";
+
+    return EXIT_SUCCESS;
+}
